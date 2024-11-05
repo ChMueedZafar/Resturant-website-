fea99265ed8adb079a0fafc5e2bf14211d16204a
chmueedzafar <chmueedzafar@gmail.com>
2024-11-05 23:26:28 +0500

    first ommit

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className='App'>
+     {/* Header Secation  */}
+
+
+    <Header></Header>
+
+
+      {/* Main Container  */}
+
+
+       {/* Menu Bottom */}
+      </div>
+);
+}
+
+export default App;
